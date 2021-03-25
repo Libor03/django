41be448853f43d48656255831ec8ca461911e641
strohli <libor@f13cybertech.com>
2021-03-25 10:14:49 +0100

    film a zanr

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import všech modelů, které obsahuje models.py
+
+from .models import *
+from django.db import models
+
+# Import metod MaxValueValidator, MinValueValidator z balíku django.core.validators
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Import metody reverse z balíku django.urls - zajistí generování URL obrácením URL patterns
+
+from django.urls import reverse
+# Registrace modelů v administraci aplikace
+
+admin.site.register(Genre)
+admin.site.register(Film)
+
+
+
+
+
