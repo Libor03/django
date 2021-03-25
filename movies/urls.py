d53b35085a7e3698eb9c74f44b4d7d94d189a99c
strohli <libor@f13cybertech.com>
2021-03-25 10:41:42 +0100

    staci dodelat stranku, admin works

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 # URL mapování - seznam URL adres pro aplikaci movies
 urlpatterns = [
     path('', views.index, name='index'),
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from movies.models import Film, Genre, Attachment
 
+
+def index(request):
+    """Metoda připravuje pohled pro domovskou stránku - šablona index.html"""
+
+    # Uložení celkového počtu filmů v databázi do proměnné num_films
+    num_films = Film.objects.all().count()
+
+    # Do proměnné films se uloží 3 filmy uspořádané podle hodnocení (sestupně)
+    films = Film.objects.order_by('-rate')[:3]
+
+    """ Do proměnné context, která je typu slovník (dictionary) uložíme hodnoty obou proměnných """
+    context = {
+        'num_films': num_films,
+        'films': films
+    }
+
+    """ Pomocí metody render vyrendrujeme šablonu index.html a předáme ji hodnoty v proměnné context k zobrazení """
+    return render(request, 'index.html', context=context)
 
 def index(request):
     """
